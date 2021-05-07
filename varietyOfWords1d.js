5b143840a053cb0d29c58d4fb465960174b91ef8
khajanawaz Nakeeb <76945260+Nakeebkhajanawaz@users.noreply.github.com>
2021-05-07 22:01:10 +0530

    variety day1

--- varietyOfWords1d.js
+++ varietyOfWords1d.js
@@ -1,3 +1,5 @@
+//How many words were used to construct this sentence. Now, don't exclude one letter words.
+
 sentence='I am a teacher and I love teaching There is nothing as more rewarding as educating and empowering people I found teaching more interesting than any other jobs Does this motivate you to be a teacher';
 
 function varietyOfWords(sentence){
