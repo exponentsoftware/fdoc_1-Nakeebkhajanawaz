2c540d3d970a43730e4ba94812564dda232eda77
khajanawaz Nakeeb <76945260+Nakeebkhajanawaz@users.noreply.github.com>
2021-05-07 21:56:24 +0530

    wordcounts day1

--- wordcount1a.js
+++ wordcount1a.js
@@ -1,3 +1,7 @@
+//Write a function which count the number of occurrence of words in a paragraph or a sentence.The function countWords takes a paragraph and two words as parameters.
+//It compare which word is most frequently occurred in the paragraph.
+
+
 const paragraph = 'I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.';
 
 function wordcounts(paragraph,love,you) {
